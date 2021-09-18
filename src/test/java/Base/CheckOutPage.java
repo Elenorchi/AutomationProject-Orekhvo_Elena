b6d42aeef8a3cd092b982de58e5d1d5ec67391f2
Elena Orekhvo <eleenoree@gmail.com>
2021-09-18 23:12:26 +0200

    Lecture_9

---
+++ src/test/java/Base/CheckOutPage.java
@@ -0,0 +1,39 @@
+package Base;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class CheckOutPage extends BasePage {
+    protected By checkoutBtn = By.id("checkout");
+    protected By firstName = By.id("first-name");
+    protected By lastName = By.id("last-name");
+    protected By zipCode = By.id("postal-code");
+    protected By continueBtn = By.id("continue");
+    protected By finish = By.id("finish");
+    protected By succesfulBuy = By.className("complete-header");
+
+    public CheckOutPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public CheckOutPage goToCheckout() {
+        click(checkoutBtn);
+        return this;
+    }
+
+    public CheckOutPage enterUserData() {
+        enter(firstName, "Elena");
+        enter(lastName, "Arekhva");
+        enter(zipCode, "222164");
+        click(continueBtn);
+        return this;
+    }
+
+    public CheckOutPage finishCheckout() {
+        click(finish);
+        return this;
+    }
+    public boolean succesfulBuying () {
+        return display(succesfulBuy);
+    }
+}
---
+++ src/test/java/Lecture_9/Task_9.java
@@ -0,0 +1,57 @@
+package Lecture_9;
+
+import Base.BasketPage;
+import Base.CheckOutPage;
+import Base.LoginPage;
+import driver.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Task_9 extends BaseTest {
+    LoginPage logPage;
+    BasketPage basketPage;
+    CheckOutPage checkout;
+
+    @BeforeClass
+    public void precondition () {
+        logPage = new LoginPage(driver);
+        basketPage = new BasketPage (driver);
+        checkout = new CheckOutPage(driver);
+    }
+
+    @Test
+    public void loginOkUser() {
+        logPage.open().Authorization("standard_user", "secret_sauce");
+        Assert.assertTrue(logPage.productsDisplayed());
+    }
+    @Test
+    public void loginLockedUser() {
+        logPage.open().Authorization("locked_out_user", "secret_sauce");
+        Assert.assertEquals(logPage.textOfError(), "Epic sadface: Sorry, this user has been locked out.");
+    }
+    @Test
+    public void loginProblemUser() {
+        logPage.open().Authorization("problem_user", "secret_sauce");
+        Assert.assertTrue(logPage.WrongImgDisplayed());
+    }
+    @Test
+    public void addingToBasket () {
+        logPage.open().Authorization("standard_user", "secret_sauce");
+        basketPage.addToBasket().openBasket();
+        Assert.assertTrue(basketPage.removeBtnDisplayed());
+    }
+    @Test
+    public void removingFromBasket () {
+        logPage.open().Authorization("standard_user", "secret_sauce");
+        basketPage.addToBasket().openBasket().removeProduct().continueShopping();
+        Assert.assertTrue(basketPage.addingButtonDisplayed());
+    }
+    @Test
+    public void checkingPayment () {
+        logPage.open().Authorization("standard_user", "secret_sauce");
+        basketPage.addToBasket().openBasket();
+        checkout.goToCheckout().enterUserData().finishCheckout();
+        Assert.assertTrue(checkout.succesfulBuying());
+    }
+}
